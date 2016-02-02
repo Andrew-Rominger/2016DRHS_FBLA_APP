7244996a6af9792658df9f01a42b01410218a728
Andrew Rominger <andrewrominger@outlook.com>
2016-02-02 01:03:05 -0800

    Added user profile page

--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/signIn.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/signIn.java
@@ -1,5 +1,6 @@
 package fbla.com.fbla_app_src;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,11 +16,14 @@ import com.backendless.BackendlessUser;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessException;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.local.UserTokenStorageFactory;
 
 public class signIn extends AppCompatActivity{
     public EditText enterUsername;
     public EditText enterPassword;
     public Button signinButton;
+    String userToken;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -31,23 +35,28 @@ public class signIn extends AppCompatActivity{
         signinButton = (Button) findViewById(R.id.signInButton);
         signinButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
-                Backendless.UserService.login(enterUsername.getText().toString().toLowerCase(), enterPassword.getText().toString().toLowerCase(), new AsyncCallback<BackendlessUser>()
-                {
-                    public void handleResponse( BackendlessUser user )
-                    {
+            public void onClick(View v) {
+
+
+                Backendless.UserService.login(enterUsername.getText().toString().toLowerCase(), enterPassword.getText().toString().toLowerCase(), new AsyncCallback<BackendlessUser>() {
+                    public void handleResponse(BackendlessUser user) {
                         // user has been logged in
-                        Toast.makeText(getApplicationContext(), "Logged In", Toast.LENGTH_LONG).show();
+                        boolean isValidLogin = Backendless.UserService.isValidLogin();
+                        if (isValidLogin) {
+                            Toast.makeText(getApplicationContext(), "Logged In", Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(signIn.this, profilePage.class);
+                            intent.putExtra("uname", enterUsername.getText().toString().toLowerCase());
+                            intent.putExtra("pword", enterPassword.getText().toString().toLowerCase());
+                            startActivity(intent);
+                        }
                     }
 
-                    public void handleFault( BackendlessFault fault )
-                    {
+                    public void handleFault(BackendlessFault fault) {
                         // login failed, to get the error code call fault.getCode()
                         Toast.makeText(getApplicationContext(), fault.getCode(), Toast.LENGTH_LONG).show();
                     }
 
-                });
+                }, true);
 
             }
         });
