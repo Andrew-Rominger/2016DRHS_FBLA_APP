64a1bf30cf5c1c15ee8a0ab17b2c1391e8972bec
WesBanghart <Wbang98@gmail.com>
2016-02-02 23:39:39 -0800

    finished extra info added finding friends activity

---
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/extrainfo.java
@@ -0,0 +1,38 @@
+package fbla.com.fbla_app_src;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class extrainfo extends AppCompatActivity {
+
+    private TextView skip;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_extrainfo);
+
+
+        skip = (TextView) findViewById(R.id.skipToFriends);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipToFriends();
+            }
+        });
+
+    }
+    public void skipToFriends()
+    {
+        Intent i = new Intent(extrainfo.this, findingfriends.class );
+        startActivity(i);
+    }
+
+}
