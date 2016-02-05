541dc6b01b936fd5a21c9cbae8eecee2f9a76209
Andrew Rominger <andrewrominger@outlook.com>
2016-02-05 00:14:20 -0800

    Added pic upload

--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/profilePage.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/profilePage.java
@@ -1,23 +1,23 @@
 package fbla.com.fbla_app_src;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
-import android.content.CursorLoader;
 import android.content.Intent;
+
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.DocumentsContract;
+
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,10 +27,10 @@ import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
 import com.backendless.files.BackendlessFile;
 
-import org.w3c.dom.Text;
-
+import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.net.URI;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 
 public class profilePage extends AppCompatActivity {
 
@@ -42,6 +42,7 @@ public class profilePage extends AppCompatActivity {
     Button loggoutButton;
     Button uploadPhoto;
     util Utility;
+
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
 
@@ -66,9 +67,24 @@ public class profilePage extends AppCompatActivity {
 
 
         unameField.setText(user.getProperty("userName").toString());
-        phoneNumField.setText(Utility.convertPhone(user.getProperty("phoneNumber").toString()));
+        if(user.getProperty("phoneNumber") == null)
+        {
+            phoneNumField.setText("No Phone number set");
+        }
+        else
+        {
+            phoneNumField.setText(Utility.convertPhone(user.getProperty("phoneNumber").toString()));
+        }
+        if(user.getProperty("name") == null)
+        {
+            nameField.setText("No name set");
+        }
+        else
+        {
+            nameField.setText(user.getProperty("name").toString());
+        }
         emailField.setText(user.getEmail());
-        nameField.setText(user.getProperty("name").toString());
+
         loggoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -97,48 +113,51 @@ public class profilePage extends AppCompatActivity {
         });
 
         }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        String realPath;
 
-            if (resultCode == Activity.RESULT_OK) {
-                // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
-            } else if (resultCode == RESULT_CANCELED) {
-                // User cancelled the image capture
-                Toast.makeText(this, "Cancled", Toast.LENGTH_LONG).show();
-            } else {
-                // Image capture failed, advise user
-                Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show();
-            }
-            if(Build.VERSION.SDK_INT < 11) {
-                realPath = Utility.getRealPathFromURI_BelowAPI11(this, data.getData());
-            }
-            else if(Build.VERSION.SDK_INT < 19)
-            {
-                realPath = Utility.getRealPathFromURI_API11to18(this, data.getData());
-            }
-            else
-            {
-                realPath = Utility.getRealPathFromURI_API19(this, data.getData());
-            }
+        String name = user.getObjectId().toString() + "_" + user.getProperty("picNum");
+        String location = "/media/userpics";
 
-            try{
-                Backendless.Files.upload(new File(realPath, user.getProperty("userName").toString() + "_" + user.getProperty("picNum")),"root\\media\\userpics");
-            }
-            catch(Exception e)
+        Bitmap photo = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.WEBP, 100 /*ignored for PNG*/, bos);
+        byte[] bitmapdata = bos.toByteArray();
+        Backendless.Files.saveFile(location, name + ".webp", bitmapdata, new AsyncCallback<String>() {
+            @Override
+            public void handleResponse(String s)
             {
-                System.out.println(e);
+                Toast.makeText(profilePage.this, s, Toast.LENGTH_LONG).show();
+                user.setProperty("picNum", ((Integer) user.getProperty("picNum") + 1));
+                Backendless.UserService.update(user, new AsyncCallback<BackendlessUser>() {
+                    @Override
+                    public void handleResponse(BackendlessUser backendlessUser) {
+
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault backendlessFault) {
+
+                    }
+                });
             }
 
+            @Override
+            public void handleFault(BackendlessFault backendlessFault)
+            {
+                Toast.makeText(profilePage.this, backendlessFault.getCode(), Toast.LENGTH_LONG).show();
+            }
+        });
+        Toast.makeText(this, "File uploaded to " + location + " as " + name, Toast.LENGTH_LONG).show();
 
 
 
 
 
     }
+
     public void moveToCamera()
     {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
