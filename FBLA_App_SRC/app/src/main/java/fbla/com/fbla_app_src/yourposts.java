e990211ba8ca24d7b63f93e5cd20054f6cb30974
WesBanghart <Wbang98@gmail.com>
2016-02-08 15:47:49 -0800

    added files

--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/yourposts.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/yourposts.java
@@ -7,12 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class yourposts extends AppCompatActivity {
+public class yourPosts extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_yourposts);
+        setContentView(R.layout.activity_your_posts);
+
 
 
 
