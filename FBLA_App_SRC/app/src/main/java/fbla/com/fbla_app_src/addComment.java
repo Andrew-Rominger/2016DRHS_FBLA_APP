b2c968dccc9b28ddc4a23dac8f478ea00c4e434f
WesBanghart <Wbang98@gmail.com>
2016-05-01 13:44:52 -0700

    fixed commment things and navigatoin stuff

--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/addComment.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/addComment.java
@@ -1,9 +1,13 @@
 package fbla.com.fbla_app_src;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -17,6 +21,7 @@ public class addComment extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
+        setupUI(findViewById(R.id.actAddCommentBg));
         add = (Button) findViewById(R.id.addCommentButton);
         input = (EditText) findViewById(R.id.commentContent);
         cancel = (Button) findViewById(R.id.cancelAddComment);
@@ -41,6 +46,39 @@ public class addComment extends AppCompatActivity
             }
         });
 
+    }
+    //this method hides the keyboard
+    public static void hideSoftKeyboard(Activity activity)
+    {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+    //this method checks to see if the user has clicked outside of the edit text when the key board is up, if they do the hideSoftKeyboard() method is called
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.
+        if(!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(addComment.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
     }
 
 
--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/searchByTags.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/searchByTags.java
@@ -1,8 +1,12 @@
 package fbla.com.fbla_app_src;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -23,7 +27,10 @@ import com.backendless.exceptions.BackendlessFault;
 import com.backendless.persistence.BackendlessDataQuery;
 import com.backendless.persistence.QueryOptions;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -58,6 +65,24 @@ public class searchByTags extends AppCompatActivity
         profile = (FrameLayout) findViewById(R.id.searchProfile);
         spinner = (ProgressBar) findViewById(R.id.loadingbartagged);
         search = (FrameLayout) findViewById(R.id.FLSearch);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(searchByTags.this, trendingsearch.class));
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBackCamera(1, searchByTags.this);
+            }
+        });
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(searchByTags.this, profilePage.class));
+            }
+        });
         i = getIntent();
         Bundle b = i.getExtras();
 
@@ -255,6 +280,21 @@ public class searchByTags extends AppCompatActivity
             return itemView;
             //return super.getView(position, convertView, parent);
         }
+    }
+    //declares image path
+    private String pictureImagePath = "";
+    public void openBackCamera(int numCode, Context context)
+    {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = timeStamp + ".png";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        pictureImagePath = storageDir.getAbsolutePath() + "/" + imageFileName;
+        File file = new File(pictureImagePath);
+        Uri outputFileUri = Uri.fromFile(file);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(cameraIntent, numCode);
     }
 
 }
