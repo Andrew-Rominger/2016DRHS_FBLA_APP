6f9bc4692c113aa9acbc3cd38160cad587b3dfa8
WesBanghart <Wbang98@gmail.com>
2016-05-01 16:48:57 -0700

    findal real

--- FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/recoverPassword.java
+++ FBLA_App_SRC/app/src/main/java/fbla/com/fbla_app_src/recoverPassword.java
@@ -1,12 +1,17 @@
 package fbla.com.fbla_app_src;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.backendless.Backendless;
@@ -23,6 +28,7 @@ public class recoverPassword extends AppCompatActivity {
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover_password);
+        setupUI(findViewById(R.id.recoverBg));
 
         usernameInput = (EditText) findViewById(R.id.usernameInput);
         submit = (Button) findViewById(R.id.submitRecover);
@@ -54,6 +60,39 @@ public class recoverPassword extends AppCompatActivity {
             }
         });
 
+    }
+    //this method hides the keyboard
+    public static void hideSoftKeyboard(Activity activity)
+    {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+    }
+    //this method checks to see if the user has clicked outside of the edit text when the key board is up, if they do the hideSoftKeyboard() method is called
+    public void setupUI(View view) {
+
+        //Set up touch listener for non-text box views to hide keyboard.
+        if(!(view instanceof EditText)) {
+
+            view.setOnTouchListener(new View.OnTouchListener() {
+
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(recoverPassword.this);
+                    return false;
+                }
+
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+
+                View innerView = ((ViewGroup) view).getChildAt(i);
+
+                setupUI(innerView);
+            }
+        }
     }
 
 }
